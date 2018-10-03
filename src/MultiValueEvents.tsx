2892e22ecaffc44682d2fbd8e0b95aab1f754b8f
Otto Streifel <ottost@microsoft.com>
2018-10-03 12:32:16 -0700

    Fix #55 Create give more helpful error message

--- src/MultiValueEvents.tsx
+++ src/MultiValueEvents.tsx
@@ -1,6 +1,7 @@
 import { initializeIcons } from "office-ui-fabric-react/lib/Icons";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { getClient } from "TFS/WorkItemTracking/RestClient";
 import { WorkItemFormService } from "TFS/WorkItemTracking/Services";
 import { getSuggestedValues } from "./getSuggestedValues";
 import { MultiValueControl } from "./MultiValueControl";
@@ -14,13 +15,21 @@ export class MultiValueEvents {
     private _fired: number = 0;
 
     public async refresh(selected?: string[]): Promise<void> {
+        let error = "";
         if (!selected) {
             if (this._fired) {
                 this._fired--;
-                return;
+                if (this._fired !== 0) {
+                    return;
+                }
+                error = await this._checkFieldType();
+                if (!error) {
+                    return;
+                }
             }
             selected = await this._getSelected();
         }
+
         ReactDOM.render(<MultiValueControl
             selected={selected}
             options={await getSuggestedValues()}
@@ -28,6 +37,7 @@ export class MultiValueEvents {
             width={this._container.scrollWidth}
             placeholder={selected.length ? "Click to Add" : "No selection made"}
             onResize={this._resize}
+            error={error}
         />, this._container, () => {
             this._resize();
             if (this._onRefreshed) {
@@ -50,9 +60,22 @@ export class MultiValueEvents {
         this.refresh(values);
         this._fired++;
         const formService = await WorkItemFormService.getService();
-        formService.setFieldValue(this.fieldName, values.join(";"));
+
+        await formService.setFieldValue(this.fieldName, values.join(";"));
+
         return new Promise<void>((resolve) => {
             this._onRefreshed = resolve;
         });
+    }
+    private async _checkFieldType(): Promise<string> {
+        const formService = await WorkItemFormService.getService();
+        const inv = await formService.getInvalidFields();
+        if (inv.length > 0 && inv.some((f) => f.referenceName === this.fieldName)) {
+            const field = await getClient().getField(this.fieldName);
+            if (field.isPicklist && VSS.getConfiguration().witInputs.Values) {
+                return `Set the field ${this.fieldName} to use suggested values rather than allowed values`;
+            }
+        }
+        return "";
     }
 }
