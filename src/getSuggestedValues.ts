ffa697e5aa98f30b58990f5d4db5b15454daa7c8
Josh <joshua.barclay@live.com.au>
2022-09-17 18:44:09 +1000

    Fix whitespace mismatch causing UI selection bug

--- src/getSuggestedValues.ts
+++ src/getSuggestedValues.ts
@@ -4,7 +4,7 @@ export async function getSuggestedValues(): Promise<string[]> {
     const inputs: IDictionaryStringTo<string> = VSS.getConfiguration().witInputs;
     const valuesString: string = inputs.Values;
     if (valuesString) {
-        return valuesString.split(";").filter((v) => !!v);
+        return valuesString.split(";").filter((v) => !!v).map(s => s.trim());
     }
     // if the values input were not specified as an input, get the suggested values for the field.
     const service = await WorkItemFormService.getService();
