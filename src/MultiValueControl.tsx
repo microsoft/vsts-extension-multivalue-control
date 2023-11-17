2ba7265776794ef08d23e1992443477137a072f8
AminTi <54841342+AminTi@users.noreply.github.com>
2023-11-17 12:54:52 +0100

    make-checkbox-values-ellipsis-revert (#283)

--- src/MultiValueControl.tsx
+++ src/MultiValueControl.tsx
@@ -106,7 +106,7 @@ export class MultiValueControl extends React.Component<IMultiValueControlProps, 
                         onFocus: this._onFocus,
                     }}
                     onChange={() => this._toggleOption(o)}
-                    label={this._wrapText(o.length > 30 ? `${o.slice(0, 30)}...` : o)}
+                    label={this._wrapText(o)}
                     title={o}
                 />)}
             </FocusZone>
