a0c13a3d49a35ca4a0199e990b19497aeefa4f26
Daria Kiritseva <dakirits@microsoft.com>
2024-10-01 15:06:15 -0700

    change comment

--- src/getSuggestedValues.ts
+++ src/getSuggestedValues.ts
@@ -11,7 +11,7 @@ export async function getSuggestedValues(): Promise<string[]> {
     const allowedValues = await service.getAllowedFieldValues(VSS.getConfiguration().witInputs.FieldName) as string[];
 
     // The getAllowedFieldValues API now returns both predefined and user-saved values,
-    // which are stored as a semicolon-separated list.
-    // Filter out values containing semicolons to prevent duplicates.
+    // which are stored as a semicolon-separated string in case multiple options are selected.
+    // Filter out user-saved values from the allowed values list to prevent them from showing up as separate options.
     return allowedValues.filter((value) => value.indexOf(";") === -1);
 }
